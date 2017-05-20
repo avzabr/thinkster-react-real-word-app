ee6a203ca8116d03e30a3d69f5ec07983ca370eb
avzabr <avzabr@gmail.com>
2017-05-20 14:47:13 +0300

    Added Provider. Updated App component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from "react";
+import {Provider} from "react-redux";
 import ReactDOM from "react-dom";
-import App from "./todo/App";
+import {createStore} from "redux";
+import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const defaultState = {
+    appName: 'conduit',
+    articles: null
+};
+
+
+const reducer = function (state = defaultState, action) {
+    return state;
+};
+
+const store = createStore(reducer);
+
+ReactDOM.render(<Provider store={store}>
+    <App/>
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
