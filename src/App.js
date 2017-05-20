a13c1622491a22707afbd76aeabc7aa4058ce146
avzabr <avzabr@gmail.com>
2017-05-20 19:27:40 +0300

    Renamed Home component to Index

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 import "./App.css";
 import {connect} from "react-redux";
 import Header from "./component/Header";
-import Home from "./component/Home/Home";
+import Home from "./component/Home/Index";
 import {Articles} from "./agent";
 
 
