018fb90c4c45917c09256a2722d3aeb680cf0ec5
avzabr <avzabr@gmail.com>
2017-05-20 18:59:21 +0300

    Added superagent

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import "./App.css";
 import {connect} from "react-redux";
 import Header from "./component/Header";
 import Home from "./component/Home/Home";
+import {Articles} from "./agent";
+
 
 const mapStateToProps = state => ({
     appName: state.appName
 });
 
 class App extends Component {
+
+    componentWillMount() {
+        Articles.all(1).then(jsn => console.log(jsn))
+    }
+
     render() {
         return (
             <div>
