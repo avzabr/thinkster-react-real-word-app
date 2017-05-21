a8110030b8a8f2ef319ea180167fc29f12c561dd
avzabr <avzabr@gmail.com>
2017-05-21 23:18:14 +0300

    Moved store to separate file

---
+++ src/store.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Aliaksandr on 21.05.2017.
+ */
+
+import {applyMiddleware, createStore} from "redux";
+import {promiseMiddleware} from "./middleware";
+
+const defaultState = {
+    appName: 'conduit',
+    articles: null
+};
+
+
+const reducer = function (state = defaultState, action) {
+    switch (action.type) {
+        case 'HOME_PAGE_LOADED':
+            return {...state, articles: action.payload.articles};
+        default:
+            return state;
+    }
+};
+
+const middleware = applyMiddleware(promiseMiddleware)
+const store = createStore(reducer, middleware);
+
+export default store;
