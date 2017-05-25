357984d89234dfc39457ffa50d402532ba513a84
avzabr <avzabr@gmail.com>
2017-05-25 22:35:11 +0300

    Added login page

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,10 +1,54 @@
-import * as React from "react";
+import React from "react";
+import {connect} from 'react-redux';
 
-export default class Login extends React.Component {
+class Login extends React.Component {
 
     render() {
         return (
-            <h1>LOGIN</h1>
+            <div className="auth-page">
+                <div className="container page">
+                    <div className="row">
+
+                        <div className="col-md-6 offset-md-3 col-xs-12">
+                            <h1 className="text-xs-center">Sign In</h1>
+                            <p className="text-xs-center">
+                                <a>
+                                    Need an account?
+                                </a>
+                            </p>
+
+                            <form>
+                                <fieldset>
+
+                                    <fieldset className="form-group">
+                                        <input
+                                            className="form-control form-control-lg"
+                                            type="email"
+                                            placeholder="Email" />
+                                    </fieldset>
+
+                                    <fieldset className="form-group">
+                                        <input
+                                            className="form-control form-control-lg"
+                                            type="password"
+                                            placeholder="Password" />
+                                    </fieldset>
+
+                                    <button
+                                        className="btn btn-lg btn-primary pull-xs-right"
+                                        type="submit">
+                                        Sign in
+                                    </button>
+
+                                </fieldset>
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         )
     }
 }
+
+export default connect(() => ({}), () => ({}))(Login);
